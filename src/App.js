54f444a256d8f9192ac880f8edd79fe77ebd824e
veroni1337 <aliserb773@gmail.com>
2022-07-17 13:40:30 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Footer from "./header";
+import Technologies from "./technologies";
+
+const App = () => {
+   return (
+       <div>
+           <Footer />
+           <Technologies />
+       </div>
+   );
+}
+
+export default App;
---
+++ src/header.js
@@ -0,0 +1,20 @@
+const  Header = () => {
+    return (
+        <div>
+            <ul>
+                <li>
+                    <a href="#"> link test </a>
+                </li>
+                <li>
+                    <a href="#"> link2 </a>
+                </li>
+
+                <li>
+                    <a href="#"> link3 </a>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Header;
